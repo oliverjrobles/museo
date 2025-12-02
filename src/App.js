836dd69f0ae917a1ae57df1a25c9ae6b09f614af
oliverjrobles <oliver.robles@live.dk>
2025-12-02 11:55:08 +0100

    main

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="min-h-screen flex items-center justify-center bg-slate-900 text-4xl text-green-500">Tailwind virker!!</div>;
+}
+
+export default App;
